b5280386c73e0e25077987d3dec28f3844ca33dc
Eklavya Mohan Agrawal <mohanagrawal25@gmail.com>
2024-10-26 19:09:51 +0530

    Backend Setup v1.0

---
+++ TasteNet/Backend/tastenet/src/main/java/com/tastenet/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tastenet.repositories;
+
+import com.tastenet.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
---
+++ TasteNet/Backend/tastenet/src/test/java/com/tastenet/TasteNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tastenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
